2cffea63dff4f1326da824ea599e9ae83ac5f6de
Vitalick Bokhan <miraigeweb@gmail.com>
2025-11-18 22:21:24 +0300

    The user created the component and rendered it for display in the browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import "./App.css";
 import { Button } from "./components/Button/Button";
+import { Users } from "./components/Users/Users";
 
 export const App = () => {
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 90,
+  };
+
+  const title = "My academic project"
+
   return (
     <div>
-      <h1>Welcome to my app</h1>
+     
+      
+      <Users 
+          users={user}
+          title={title}    
+      />
+
       <Button title="Button" />
+
+      <div className="avatar_box">
+        <img className="avatar" alt="" />
+      </div>
     </div>
   );
 };
---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,35 @@
+import type { FC } from "react";
+
+export type UserType = {
+  name: string;
+  imageUrl: string;
+  imageSize: number;
+};
+export type UsersType = {
+  title: string
+  users: UserType
+};
+
+
+export const Users: FC<UsersType> = 
+({
+  title,
+  users,
+
+}) => {
+  
+  return (
+    <div className="">
+        <h2 className="name">{title}</h2>
+          <img 
+            src={users.imageUrl} 
+            alt={'Photo of ' + users.name}
+            style={{
+              width: users.imageSize,
+              height: users.imageSize
+            }}  
+          />
+          <div>{'Photo of ' + users.name}</div>
+    </div>
+  );
+};
