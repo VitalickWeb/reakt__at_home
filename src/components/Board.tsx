51435f34267b1f572f74f6acfcc977b7bcfdb13b
Vitalick Bokhan <miraigeweb@gmail.com>
2025-12-11 20:53:48 +0300

    wrote a function to check the winner's announcement.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,7 +9,7 @@ export const Board: FC<BoardType> = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     const addValueInSquare = (i: number) => {
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
 
@@ -69,4 +69,28 @@ export const Board: FC<BoardType> = () => {
             </div>
         </>
     );
+};
+
+const calculateWinner = (squares: string[]) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
 };
